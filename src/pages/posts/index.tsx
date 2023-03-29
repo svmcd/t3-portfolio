89693a3df0c27b2c672ce10c66f8d4f2f38c3c64
svmcd <spolat0750@gmail.com>
2023-03-29 11:55:43 +0200

    added next image

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,5 @@
 import { type NextPage } from "next";
+import Image from "next/image";
 import { api, RouterOutputs } from "@component/utils/api";
 
 type Post = RouterOutputs["posts"]["getAll"][number];
@@ -17,8 +18,9 @@ const PostsPage: NextPage = () => {
   return (
     <>
       {posts?.map((post: Post) => (
-        <div key={post.id}>
+        <div key={post.id} className="round my-4 bg-slate-800 p-4">
           <div key={post.id}>{post.title}</div>
+          <Image src={post.image} alt={post.title} width={56} height={56} />
           <p>{post.content}</p>
           <p>{post.year}</p>
         </div>
