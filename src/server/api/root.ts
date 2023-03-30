69730dfe91d53ce8ca518af2b42b25261ae1b973
samed <spolat0750@gmail.com>
2023-03-30 17:00:25 +0200

    posting & auth (#1)
    * added back auth & post model
    
    * postsRouter
    
    * update root
    
    * deleted example
    
    * rendered posts
    
    * removed unnecessary imports
    
    * added posts component
    
    * CreatePostWizard, added some styling so its better for the eyes
    
    * modified model, added Post type
    
    * added next image
    
    * added form structure
    
    * kinda working form
    
    * fixed type error?
    
    * fixed server error
    
    * orderBy, map trough object entries so less repetitive code
    
    * defaultFormData
    
    * fixed typerror
    
    * post creation is now on a seperate page
    
    * wip image upload
    
    * fix
    
    * gitignnore
    
    * vercel
    
    * added auth
    
    * auth

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "@component/server/api/trpc";
-import { exampleRouter } from "@component/server/api/routers/example";
+import { postsRouter } from "@component/server/api/routers/posts";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "@component/server/api/routers/example";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  posts: postsRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,30 @@
+import { createTRPCRouter, publicProcedure } from "@component/server/api/trpc";
+import { z } from "zod";
+
+export const postsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.post.findMany({
+      orderBy: [{ date: "desc" }],
+    });
+  }),
+
+  create: publicProcedure
+    .input(
+      z.object({
+        title: z.string().optional(),
+        content: z.string().optional(),
+        imageUrl: z.string().optional(),
+        technologies: z.string().optional(),
+        date: z.string().optional(),
+        link1: z.string().optional(),
+        link2: z.string().optional(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const post = await ctx.prisma.post.create({
+        data: input,
+      });
+
+      return post;
+    }),
+});
