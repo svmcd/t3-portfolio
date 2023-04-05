563a2ebd4b0bd99226c2a177dbfee6cab74d328d
svmcd <spolat0750@gmail.com>
2023-04-05 13:21:46 +0200

    fixed logo class not updating

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Header = ({ theme }: Record<string, unknown>) => {
+  const [logoClass, setLogoClass] = useState("");
+
+  useEffect(() => {
+    const newLogoClass =
+      theme === "light" ? "mix-blend-multiply invert" : "mix-blend-screen";
+    setLogoClass(newLogoClass);
+  }, [theme]);
+
   return (
     <>
       <div className="pointer-events-none absolute top-0 left-0 h-20	select-none">
@@ -10,11 +19,7 @@ const Header = ({ theme }: Record<string, unknown>) => {
           height={200}
           width={200}
           unoptimized={true}
-          className={` ${
-            theme === "light"
-              ? " mix-blend-multiply invert"
-              : "	mix-blend-screen"
-          }`}
+          className={logoClass}
         />
         <p>Developer & Designer</p>
       </div>
