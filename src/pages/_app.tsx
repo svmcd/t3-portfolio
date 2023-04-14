2ab7e99c30f8f397b480fd12481c8a12a004593a
svmcd <spolat0750@gmail.com>
2023-04-14 11:56:35 +0200

    refactor: removed prisma, trpc and nextAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,17 @@
 import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from "next-themes";
-
-import { api } from "@component/utils/api";
 
 import "@component/styles/globals.css";
-
+import { ThemeProvider } from "next-themes";
 import { Main } from "@component/components/Main";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
-    <SessionProvider session={session}>
-      <ThemeProvider attribute="class">
-        <Main>
-          <Component {...pageProps} />
-        </Main>
-      </ThemeProvider>
-    </SessionProvider>
+    <ThemeProvider attribute="class">
+      <Main>
+        <Component {...pageProps} />
+      </Main>
+    </ThemeProvider>
   );
 };
 
-export default api.withTRPC(MyApp);
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,10 @@
 import { type NextPage } from "next";
-import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 const Home: NextPage = () => {
-  const { data: sessionData } = useSession();
-
   return (
     <>
-      <div className="flex h-full flex-col items-end justify-end">
-        <button
-          onClick={sessionData ? () => void signOut() : () => void signIn()}
-        >
-          {sessionData ? "Sign out" : "Sign in"}
-        </button>
-        <Link href="/projects">View projects</Link>
-        <Link href="/createProject">Create project</Link>
-      </div>
+      <div className="flex h-full flex-col items-end justify-end"></div>
     </>
   );
 };
