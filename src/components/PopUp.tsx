1f8602001a491cf3364c41a986a7c17193759662
svmcd <spolat0750@gmail.com>
2023-06-01 10:36:09 +0200

    dialog -> div

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -5,19 +5,22 @@ import { motion } from "framer-motion";
 export const PopUp = () => {
   const [popUpStatus, setPopUpStatus] = useState(true);
 
+  useEffect(() => {
+    console.log(popUpStatus);
+  }, [popUpStatus]);
+
   return (
     <>
-      <dialog
-        open={popUpStatus}
-        className="w-fullsm:w-4/5 z-30 box-border h-40 bg-amber-500 dark:bg-amber-300 md:w-2/3"
-      >
-        <button
-          className="cursor-pointer"
-          onClick={() => setPopUpStatus(false)}
-        >
-          x
-        </button>
-      </dialog>
+      <div className="absolute inset-0 flex justify-center">
+        <div className="z-30 h-40 w-full bg-amber-500 dark:bg-amber-300 sm:w-4/5">
+          <button
+            className="cursor-pointer"
+            onClick={() => setPopUpStatus(false)}
+          >
+            x
+          </button>
+        </div>
+      </div>
     </>
   );
 };
