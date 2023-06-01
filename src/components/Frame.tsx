b88ad7cfca34e3082a565e9844e075a275d21080
svmcd <spolat0750@gmail.com>
2023-06-01 16:58:27 +0200

    responsive

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,10 +1,10 @@
 export const Frame = () => {
   return (
     <div className="bg-red pointer-events-none absolute z-40 h-full w-full bg-transparent ">
-      <div className="bg-white-500 bg-opacity-15 absolute top-0 h-8 w-full bg-clip-padding backdrop-blur-sm backdrop-filter"></div>
-      <div className="bg-white-500 bg-opacity-15 absolute bottom-0 h-8 w-full bg-clip-padding backdrop-blur-sm backdrop-filter"></div>
-      <div className="bg-white-500 bg-opacity-15 absolute left-0 h-full w-8 bg-clip-padding backdrop-blur-sm backdrop-filter"></div>
-      <div className="bg-white-500 bg-opacity-15 absolute right-0 h-full w-8 bg-clip-padding backdrop-blur-sm backdrop-filter"></div>
+      <div className="bg-white-500 bg-opacity-15 absolute top-0 h-[1.4rem] w-full bg-clip-padding backdrop-blur-sm backdrop-filter md:h-8"></div>
+      <div className="bg-white-500 bg-opacity-15 absolute bottom-0 h-[1.4rem] w-full bg-clip-padding backdrop-blur-sm backdrop-filter md:h-8"></div>
+      <div className="bg-white-500 bg-opacity-15 absolute left-0 h-full w-[1.4rem] bg-clip-padding backdrop-blur-sm backdrop-filter md:w-8"></div>
+      <div className="bg-white-500 bg-opacity-15 absolute right-0 h-full w-[1.4rem] bg-clip-padding backdrop-blur-sm backdrop-filter md:w-8"></div>
     </div>
   );
 };
