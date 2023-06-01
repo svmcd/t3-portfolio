2ec0780edca122d83e7d61a3dc90247b11e4281c
svmcd <spolat0750@gmail.com>
2023-06-01 16:14:31 +0200

    minor changes

--- src/pages/projects/[projectId].tsx
+++ src/pages/projects/[projectId].tsx
@@ -44,23 +44,28 @@ const Project = () => {
                 ?.toUpperCase()
                 .replace(/ /g, "\u00A0 \u00A0 \u00A0") ??
               "no technologies specified"
-            }\u00A0 \u00A0 \u00A0`.repeat(5)}
+            }\u00A0 \u00A0 \u00A0`.repeat(10)}
           </Typography>
         </motion.div>
-        <Typography variant="text">{foundProject?.content}</Typography>
+        <Typography variant="text">
+          {foundProject?.content ??
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+        </Typography>
         <Typography variant="heading">{foundProject?.date}</Typography>
         <div className="flex items-center justify-end gap-2">
-          {foundProject?.link1 && (
+          {(foundProject?.link1 && (
             <Link target="_blank" href={foundProject.link1}>
               <Typography variant="text">Live</Typography>
             </Link>
-          )}
+          )) ??
+            "n/a"}
           <span>•</span>
-          {foundProject?.link2 && (
+          {(foundProject?.link2 && (
             <Link target="_blank" href={foundProject.link2}>
-              <Typography variant="text">GitHub</Typography>
+              <Typography variant="text">Code</Typography>
             </Link>
-          )}
+          )) ??
+            "n/a"}
         </div>
       </div>
     </Motion>
