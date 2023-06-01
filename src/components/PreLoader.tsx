b3bf359d836f964fe06ef9d1ade79edceaebf29f
svmcd <spolat0750@gmail.com>
2023-06-01 12:13:45 +0200

    fixed ful reoload

--- src/components/PreLoader.tsx
+++ src/components/PreLoader.tsx
@@ -5,18 +5,17 @@ export const PreLoader = () => {
   const [preLoaderClass, setPreLoaderClass] = useState("opacity-100");
 
   useEffect(() => {
-    setTimeout(() => {
+    const timeout = setTimeout(() => {
       setPreLoaderClass("opacity-0");
-      setTimeout(() => {
-        setPreLoaderClass("hidden");
-      }, 2100);
     }, 2000);
+
+    return () => clearTimeout(timeout);
   }, []);
 
   return (
     <>
       <div
-        className={`absolute inset-0 z-40 flex items-center justify-center bg-stone-200 transition-opacity duration-1000 dark:bg-dark ${preLoaderClass}`}
+        className={`absolute inset-0 z-40 flex items-center justify-center bg-stone-200 transition-opacity duration-500 dark:bg-dark ${preLoaderClass}`}
       >
         <Typography variant="title">Samed Polat</Typography>
       </div>
