2042616f11771db772720df20c50d422536f30e2
svmcd <spolat0750@gmail.com>
2023-06-06 16:54:32 +0200

    some ui changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,36 +1,119 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { Typography } from "@component/components/Typography";
+import { useRouter } from "next/router";
+import { faSquareFull } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const Square = () => {
+  return <FontAwesomeIcon icon={faSquareFull} className="text-[.5rem]" />;
+};
 
 export const Sidebar = () => {
   const { data: sessionData } = useSession();
+  const router = useRouter();
 
   return (
     <div className="flex flex-col items-start gap-2">
       <Link href="/">
-        <Typography variant="text" clickable>
-          Home
+        <Typography
+          variant="text"
+          clickable
+          className={
+            router.pathname == "/"
+              ? "mx-2 flex items-center gap-1 opacity-40"
+              : ""
+          }
+        >
+          {router.pathname == "/" ? (
+            <>
+              <Square />
+              {"Home"}
+            </>
+          ) : (
+            "Home"
+          )}
         </Typography>
       </Link>
       <Link href="/projects">
-        <Typography variant="text" clickable>
-          View projects
+        <Typography
+          variant="text"
+          clickable
+          className={
+            router.pathname == "/projects"
+              ? "mx-2 flex items-center gap-1 opacity-40"
+              : ""
+          }
+        >
+          {router.pathname == "/projects" ? (
+            <>
+              <Square />
+              {"Projects"}
+            </>
+          ) : (
+            "Projects"
+          )}
         </Typography>
       </Link>
       <Link href="/experience">
-        <Typography variant="text" clickable>
-          Experience
+        <Typography
+          variant="text"
+          clickable
+          className={
+            router.pathname == "/experience"
+              ? "mx-2 flex items-center gap-1 opacity-40"
+              : ""
+          }
+        >
+          {router.pathname == "/experience" ? (
+            <>
+              <Square />
+              {"Experience"}
+            </>
+          ) : (
+            "Experience"
+          )}
         </Typography>
       </Link>
       <Link href="/contact">
-        <Typography variant="text" clickable>
-          Contact
+        <Typography
+          variant="text"
+          clickable
+          className={
+            router.pathname == "/contact"
+              ? "mx-2 flex items-center gap-1 opacity-40"
+              : ""
+          }
+        >
+          {router.pathname == "/contact" ? (
+            <>
+              <Square />
+              {"Contact"}
+            </>
+          ) : (
+            "Contact"
+          )}
         </Typography>
       </Link>
       {sessionData ? (
         <Link href="/createProject">
-          <Typography variant="text" clickable>
-            Create project
+          <Typography
+            variant="text"
+            clickable
+            className={
+              router.pathname == "/createProject"
+                ? "mx-2 flex items-center gap-1"
+                : ""
+            }
+          >
+            {router.pathname == "/createProject" ? (
+              <>
+                <Square />
+                {"Create Project"}
+              </>
+            ) : (
+              "Create Project"
+            )}
           </Typography>
         </Link>
       ) : (
--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -4,6 +4,7 @@ interface TypographyProps {
   variant: "title-big" | "title" | "heading" | "text" | "text-light";
   clickable?: boolean;
   bold?: boolean;
+  className?: string;
   children: React.ReactNode;
 }
 
@@ -11,42 +12,47 @@ export const Typography: FC<TypographyProps> = ({
   variant,
   clickable,
   bold,
+  className,
   children,
 }) => {
-  let className = "";
+  let baseClassName = "";
   let hoverClassName = "";
   let fontWeight = "";
+
   switch (variant) {
     case "title-big":
-      className = className =
-        "f-light text-3xl  sm:text-5xl text-stone-900 dark:text-stone-300";
+      baseClassName =
+        "f-light text-3xl sm:text-5xl text-stone-900 dark:text-stone-300";
       break;
     case "title":
-      className = className =
-        "f-light text-2xl  sm:text-4xl text-stone-900 dark:text-stone-300";
+      baseClassName =
+        "f-light text-2xl sm:text-4xl text-stone-900 dark:text-stone-300";
       break;
     case "heading":
-      className = "f-light text-lg text-stone-900 dark:text-stone-300";
+      baseClassName = "f-light text-lg text-stone-900 dark:text-stone-300";
       break;
     case "text":
-      className =
+      baseClassName =
         "text-xs sm:text-sm f-bold text-stone-900 dark:text-stone-300";
       break;
     case "text-light":
-      className = "text-sm f-light text-stone-900 dark:text-stone-300";
+      baseClassName = "text-sm f-light text-stone-900 dark:text-stone-300";
       break;
     default:
-      className = "text-sm text-stone-900 dark:text-stone-300";
+      baseClassName = "text-sm text-stone-900 dark:text-stone-300";
   }
+
   if (clickable) {
     hoverClassName = "hover:opacity-40 duration-300";
   }
+
   if (bold) {
     fontWeight = "font-[500]";
   }
-  return (
-    <div className={`${className} ${fontWeight} ${hoverClassName}`}>
-      {children}
-    </div>
-  );
+
+  const combinedClassName = `${baseClassName} ${fontWeight} ${hoverClassName} ${
+    className || ""
+  }`;
+
+  return <div className={combinedClassName}>{children}</div>;
 };
