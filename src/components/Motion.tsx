9e7df2d24991aa5596d5dd84722d234caa262b6a
svmcd <spolat0750@gmail.com>
2023-06-18 20:42:15 +0200

    mobile vh fix

--- src/components/Motion.tsx
+++ src/components/Motion.tsx
@@ -8,7 +8,7 @@ export const Motion: FC<PropsWithChildren> = ({ children }) => {
         initial={{ opacity: 0, x: 100 }}
         animate={{ opacity: 1, x: 0 }}
         exit={{ opacity: 0, x: -100 }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 0.75 }}
       >
         {children}
       </motion.div>
